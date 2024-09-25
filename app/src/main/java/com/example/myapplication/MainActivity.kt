88214b251b3904e926624416a6f55f407805bdbc
GatoMantequilla <d.diaz17@ufromail.com>
2024-09-25 18:39:08 -0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    lateinit var btnEnter: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val btnEnter= findViewById<Button>(R.id.btn)
+        btnEnter.setOnClickListener{
+            val text = btnEnter.text
+            btnEnter.text = "click en "+ text
+            println("click en " + text)
+            println("se hizo el cambio")
+        }
+
+    }
+}
